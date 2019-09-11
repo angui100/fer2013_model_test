df9beec3fa489be42c088c11e237050bf9dea366
Andrew <zou.angui100@gmail.com>
2019-09-11 11:47:52 -0500

    Updated the test program for real time model prediction analysis

--- do_test.py
+++ do_test.py
@@ -11,13 +11,17 @@ from keras.optimizers import Adam
 from keras.regularizers import l2
 from keras.callbacks import ReduceLROnPlateau, TensorBoard, EarlyStopping, ModelCheckpoint
 from keras.models import load_model
+#from tabulate import tabulate
 
 #BASEPATH = 'drive/Emotion Recognition'
 #sys.path.insert(0, BASEPATH)
 #os.chdir(BASEPATH)
 MODELPATH = './models/model.h5'
 
-emotion_dict = {0: "Angry", 1: "Disgust", 2: "Fear", 3: "Happy", 4: "Sad", 5: "Surprise", 6: "Neutral"}
+#emotion_dict = {0: "Angry", 1: "Disgust", 2: "Fear", 3: "Happy", 4: "Sad", 5: "Surprise", 6: "Neutral"}
+#emotion_dict = {0: "Angry", 1: "Fear", 2: "Surprise", 3: "Calm"}
+emotion_dict = {0: "Angry", 1: "Fear/Surprise", 2: "Calm"}
+header = ["Angry", "Fear OR Surprise", "Calm"]
 
 model = load_model(MODELPATH)
 
@@ -37,8 +41,25 @@ while True:
         cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
         cv2.normalize(cropped_img, cropped_img, alpha=0, beta=1, norm_type=cv2.NORM_L2, dtype=cv2.CV_32F)
         prediction = model.predict(cropped_img)
-        cv2.putText(frame, emotion_dict[int(np.argmax(prediction))], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+        print(*header, sep='              ')
+        #prediction[0] = "%.2f" % prediction[0]
+        formatted_list = []
+        for item in prediction[0]:
+            formatted_list.append('{percent:.2%}'.format(percent=item))
+            #formatted_list.append("%.2f%%" %item)
+        #'{percent:.2%}'.format(percent=1.0/3.0)
+        print(*formatted_list, sep='\t\t\t')
+        
+        #print(tabulate(*formatted_list, headers=header))
+        #print(['%.1f%%' % x*100 for x in prediction[0]])
+        #print('%s: %.1f%%' % (emotion, normalized_prediction[self.emotion_map[emotion]]*100))
+        #print(int(np.argmax(prediction)))
 
+        cv2.putText(frame, emotion_dict[int(np.argmax(prediction))], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+        
+        print("Emotion: {}, {}".format(int(np.argmax(prediction)), emotion_dict[int(np.argmax(prediction))]))
+
+        print('--------------------------------------------------')
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
